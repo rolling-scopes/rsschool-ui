7a7bd830e354b4e45617d42218381161e9951d51
Konstantin <k.matush@yandex.com>
2018-08-15 18:02:43 +0300

    refactor(loading): improve checker for loadings

--- src/app/core/selectors/index.ts
+++ src/app/core/selectors/index.ts
@@ -1,24 +1,8 @@
-import { mapValues, mapKeys, isObject } from 'lodash';
-
-import { RootState } from 'core/reducers';
-
-export function isAnyPartLoaded({ router, ...state }: RootState) {
-    let isLoading = false;
-
-    function traverseState(obj: any, cb: any) {
-        mapValues(mapKeys(obj, cb), (val: any) => {
-            if (isLoading) {
-                return;
-            }
-            return isObject(val) ? traverseState(val, cb) : val;
-        });
-    }
-
-    traverseState(state, (value: any, key: any) => {
-        if (key === 'isLoading' && value) {
-            isLoading = true;
+export function isAnyPartLoaded({ router, ...state }: any) {
+    for (const key in state) {
+        if (!!state[key].isLoading) {
+            return true;
         }
-    });
-
-    return isLoading;
+    }
+    return false;
 }
