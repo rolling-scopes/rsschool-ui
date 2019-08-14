214392f6a4aee166c73fe96ff52c509a88ef782a
Andrei Palchys <andrei_palchys@epam.com>
2019-08-14 14:56:34 +0300

    fix: remove console.log

--- src/pages/course/mentor/check-task.tsx
+++ src/pages/course/mentor/check-task.tsx
@@ -59,7 +59,6 @@ class TaskScorePage extends React.Component<Props, State> {
   onChangeTask = async (value: any) => {
     const courseTaskId = Number(value);
     const courseTask = this.state.courseTasks.find(t => t.courseTaskId === courseTaskId);
-    console.log(courseTask);
     if (courseTask == null || this.allStudents == null) {
       return;
     }
